33277186c43e184987d5e609387ae05e933f79b5
FxXyz11 <312106790+FxXyz11@users.noreply.github.com>
2026-08-03 01:05:26 +0800

    Replace app icon artwork

--- pwa-assets.config.ts
+++ pwa-assets.config.ts
@@ -1,9 +1,16 @@
 import {
   defineConfig,
-  minimal2023Preset as preset,
+  minimal2023Preset,
 } from '@vite-pwa/assets-generator/config'
+
+const preset = {
+  ...minimal2023Preset,
+  transparent: { ...minimal2023Preset.transparent, padding: 0 },
+  maskable: { ...minimal2023Preset.maskable, padding: 0 },
+  apple: { ...minimal2023Preset.apple, padding: 0 },
+}
 
 export default defineConfig({
   preset,
-  images: ['assets/brand/app-icon-source.png'],
+  images: ['assets/brand/app-icon-source.jpg'],
 })
